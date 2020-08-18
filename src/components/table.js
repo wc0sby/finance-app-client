606b960985c3fd39c3ef06d88bc9da5506e16add
wc0sby <wcosby6607@gmail.com>
2020-08-17 23:43:26 -0500

    adds filter method for active categories

--- src/components/table.js
+++ src/components/table.js
@@ -4,10 +4,11 @@ export default class Table extends Component{
   getCategories(){
     const {user} = this.props
     if (user.categories){
+      const filterArr = user.categories.filter(i=>i.isActive)
       return <ul>
         {
-          user.categories.map((i,k)=>{
-            return <li key={k}>{i.category}</li>
+          filterArr.map((i,k)=>{
+            return <li key={k}>{i.category}</li> 
           })
       }
       </ul> 
@@ -16,7 +17,8 @@ export default class Table extends Component{
   render(){
     return(
       <div>
-        You are authenticated!
+        <h3>{`Welcome ${this.props.user.name}!`}</h3>
+        <h5>Below are your categories!</h5>
         {this.getCategories()}
       </div>
     )
